0f9bc015095032cc373be4effe3542992c33ac43
Rostislav Wolny <rosta.wolny@automattic.com>
2022-02-18 09:58:40 +0100

    Add dummy panels to document sidebar

--- mailpoet/assets/js/src/newsletter_editor_v2/newsletter_editor.tsx
+++ mailpoet/assets/js/src/newsletter_editor_v2/newsletter_editor.tsx
@@ -1,8 +1,26 @@
 /* eslint-disable react/react-in-jsx-scope */
 import { render } from '@wordpress/element';
-import IsolatedBlockEditor from '@automattic/isolated-block-editor';
+import { Panel, PanelBody, Button } from '@wordpress/components';
+import IsolatedBlockEditor, { DocumentSection, ToolbarSlot } from '@automattic/isolated-block-editor';
 
-const settings = {};
+const settings = {
+  iso: {
+    toolbar: {
+      inserter: true,
+      inspector: true,
+      navigation: true,
+      toc: true,
+      documentInspector: true,
+    },
+    moreMenu: {
+      editor: true,
+      fullscreen: true,
+      preview: true,
+      topToolbar: true,
+    },
+    allowApi: true,
+  },
+};
 const saveContent = (html) => (console.log(html)); // eslint-disable-line no-console
 const loadInitialContent = (parse) => {
   const html = '<!-- wp:paragraph -->\n'
@@ -17,6 +35,21 @@ render(
     onSaveContent={(html) => saveContent(html)}
     onLoad={loadInitialContent}
     onError={() => document.location.reload()}
-  />,
+  >
+    <ToolbarSlot>
+      <Button>Save Draft</Button>
+      <Button variant="primary">Send</Button>
+    </ToolbarSlot>
+    <DocumentSection>
+      <Panel>
+        <PanelBody title="Sending Settings">
+          <li>Here comes sending settings</li>
+        </PanelBody>
+        <PanelBody title="Style Settings">
+          <li>Here comes style settings</li>
+        </PanelBody>
+      </Panel>
+    </DocumentSection>
+  </IsolatedBlockEditor>,
   document.querySelector('#mailpoet-email-editor')
 );
