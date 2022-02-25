e9f5afa7e6ace53653c07ec714917896f99dafe2
John Oleksowicz <johnoleksowicz@icloud.com>
2022-02-25 15:36:33 -0600

    Only defer requests
    [MAILPOET-4018]

--- mailpoet/assets/js/src/segments/dynamic/subscribers_counter.tsx
+++ mailpoet/assets/js/src/segments/dynamic/subscribers_counter.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef } from 'react';
 import MailPoet from 'mailpoet';
 import { useSelect, useDispatch } from '@wordpress/data';
-import { debounce } from 'lodash';
 import { isFormValid } from './validator';
 import { loadCount } from './subscribers_calculator';
 
@@ -24,62 +23,53 @@ const SubscribersCounter: React.FunctionComponent = () => {
   const { updateSubscriberCount } = useDispatch('mailpoet-dynamic-segments-form');
 
   const serializedSegment = JSON.stringify(segment);
-  const latestRequestIdRef = useRef(1);
   const deferredRequestRef = useRef(null);
   const isRequestInFlight = useRef(false);
 
-  function load(loadItem: Segment): void {
-    // Don't allow multiple in-flight requests to avoid hammering the database
-    // when we'll only ever use the results of the last request
-    if (isRequestInFlight.current) {
-      deferredRequestRef.current = loadItem;
-      return;
-    }
-
-    deferredRequestRef.current = null;
-    latestRequestIdRef.current += 1;
-    const requestId = latestRequestIdRef.current;
-    isRequestInFlight.current = true;
-
-    loadCount(loadItem).then((response) => {
-      isRequestInFlight.current = false;
-      if (deferredRequestRef.current) {
-        load(deferredRequestRef.current);
-        return;
-      }
-      if (requestId !== latestRequestIdRef.current) {
-        // Don't do anything with the response because a newer request has been initiated
-        return;
-      }
-      const finished = {} as SubscriberCount;
-      finished.loading = false;
-      if (response) {
-        finished.count = response.count;
-        finished.errors = response.errors;
-      }
-      updateSubscriberCount(finished);
-    }, (errorResponse) => {
-      isRequestInFlight.current = false;
-      const finished = {} as SubscriberCount;
-      const errors = errorResponse.errors.map((error) => error.message);
-      finished.loading = false;
-      finished.count = undefined;
-      finished.errors = errors;
-      updateSubscriberCount(finished);
-    });
-  }
-
-  const debouncedLoadRef = useRef(debounce(load, 2000, { trailing: true }));
-
   useEffect(() => {
-    if (isFormValid(segment.filters)) {
+    function load(loadItem: Segment): void {
+      // Don't allow multiple in-flight requests to avoid hammering the database
+      // when we'll only ever use the results of the last request
+      if (isRequestInFlight.current) {
+        deferredRequestRef.current = loadItem;
+        return;
+      }
+
       updateSubscriberCount({
         loading: true,
         count: undefined,
         errors: undefined,
       });
-      const debouncedLoad = debouncedLoadRef.current;
-      debouncedLoad(segment);
+
+      deferredRequestRef.current = null;
+      isRequestInFlight.current = true;
+
+      loadCount(loadItem).then((response) => {
+        isRequestInFlight.current = false;
+        if (deferredRequestRef.current) {
+          load(deferredRequestRef.current);
+          return;
+        }
+        const finished = {} as SubscriberCount;
+        finished.loading = false;
+        if (response) {
+          finished.count = response.count;
+          finished.errors = response.errors;
+        }
+        updateSubscriberCount(finished);
+      }, (errorResponse) => {
+        isRequestInFlight.current = false;
+        const finished = {} as SubscriberCount;
+        const errors = errorResponse.errors.map((error) => error.message);
+        finished.loading = false;
+        finished.count = undefined;
+        finished.errors = errors;
+        updateSubscriberCount(finished);
+      });
+    }
+
+    if (isFormValid(segment.filters)) {
+      load(segment);
     } else {
       updateSubscriberCount({
         count: undefined,
