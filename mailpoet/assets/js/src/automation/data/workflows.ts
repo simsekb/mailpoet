6ab4a6e2d68bd7384bda463a471b02e0a9e1cec6
Jan Jakes <jan.jakes@automattic.com>
2022-04-27 13:28:31 +0200

    Make workflow response data typed
    [MAILPOET-4071]

--- mailpoet/assets/js/src/automation/data/workflows.ts
+++ mailpoet/assets/js/src/automation/data/workflows.ts
@@ -3,6 +3,16 @@ import useSWR from 'swr';
 
 const url = `${API_URL}/workflows`;
 
-export const getWorkflows = createFetcher();
+type ResponseData = {
+  data: {
+    id: string;
+    name: string;
+    status: 'inactive' | 'active';
+    created_at: string;
+    updated_at: string;
+  }[];
+};
+
+export const getWorkflows = createFetcher<ResponseData>();
 
 export const useWorkflowsQuery = () => useSWR(url, getWorkflows);
